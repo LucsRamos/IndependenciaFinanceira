0e8c2e936aea6d0f848aa337a954f7cea4657cf7
Lucas Ramos <lucasramos.lr260@gmail.com>
2022-09-24 01:27:46 -0300

    Adiciona a solução do exercício

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿decimal GastosMensais, ValorAInvestir;
+double dividendYield;
+Console.WriteLine("Vivendo de Dividendos\n");
+Console.Write("Gastos Mensais (em R$) : ");
+GastosMensais = Convert.ToDecimal(Console.ReadLine());
+Console.Write("Rendimentos Mensais (%) : ");
+dividendYield = Convert.ToDouble(Console.ReadLine());
+ValorAInvestir = GastosMensais / Convert.ToDecimal(dividendYield / 100);
+Console.WriteLine($"\nPara receber {GastosMensais:C} por mês com rendimentos de {dividendYield:N1}% você precisa ter investido {ValorAInvestir:C}.");
